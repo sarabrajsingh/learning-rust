e746651c56d6e37c375185b388fdc54050bf13e9
Sarabraj Singh <singh.sarabraj@gmail.com>
2022-10-27 00:33:14 -0400

    chapter-14

---
+++ chapter+14/add/add-two/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn add_two(x : i32) -> i32 {
+    x + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(8, add_two(6));
+    }
+}
